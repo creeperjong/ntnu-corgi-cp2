afece2e83eccaa000abad5e6372746be1826d17c
creeperjong <leogood50203@gmail.com>
2021-04-18 17:50:37 +0800

    init

---
+++ hw03/hw0303.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct {
+    char		bm[2];
+    uint32_t	size;
+    uint32_t	reserve;
+    uint32_t	offset;
+    uint32_t	header_size;
+    uint32_t	width;
+    uint32_t	height;
+    uint16_t	planes;
+    uint16_t	bpp;
+    uint32_t	compression;
+    uint32_t	bitmap_size;
+    uint32_t	hres;
+    uint32_t	vres;
+    uint32_t	used;
+    uint32_t	important;
+}__attribute__ ((packed)) sHeader;
+
+int main(){
+
+    FILE* origin_bmp = NULL;
+    FILE* distort_bmp = NULL;
+    char input_filename[32] = "";
+    char output_filename[32] = "";
+    int32_t ang = 0;
+    int32_t shift = 0;
+    int32_t adjust = 0;
+    int32_t pixel_per_offset = 0;
+    sHeader header = {0};
+
+    printf("Please input a BMP file: ");
+    fgets(input_filename,32,stdin);
+    input_filename[strlen(input_filename) - 1] = 0;
+    printf("Please input the output BMP file name: ");
+    fgets(output_filename,32,stdin);
+    output_filename[strlen(output_filename) - 1] = 0;
+
+    if(strncmp(&input_filename[strlen(input_filename) - 4],".bmp",4) != 0 || strncmp(&output_filename[strlen(output_filename) - 4],".bmp",4) != 0){
+        printf("Error: The file name extension is not \".bmp\"\n");
+        return 0;
+    }
+    if((origin_bmp = fopen(input_filename,"rb")) == NULL){
+        perror("Error");
+        return 0;
+    }
+    if((distort_bmp = fopen(output_filename,"wb")) == NULL){
+        perror("Error");
+        return 0;
+    }
+
+    printf("Angle (0-90): ");
+    scanf("%d",&ang);
+
+    if(ang == 0){
+        
+        uint8_t* row_modified = NULL;
+        int32_t col_modified = 0;
+        
+        fread(&header,sizeof(sHeader),1,origin_bmp);
+        header.width *= 2;
+        header.height = 1;
+        fwrite(&header,sizeof(sHeader),1,distort_bmp);
+
+        col_modified = header.width * 3 + header.width % 4;
+        row_modified = (uint8_t*)malloc(sizeof(uint8_t) * col_modified);
+        for(int32_t i = 0;i < col_modified;i++) row_modified[i] = 0;
+
+        fwrite(row_modified,1,col_modified,distort_bmp);
+        return 0;
+    }
+
+    if(ang == 90){
+
+        fread(&header,sizeof(sHeader),1,origin_bmp);
+        fwrite(&header,sizeof(sHeader),1,distort_bmp);
+
+        while(!feof(origin_bmp)){
+            uint8_t row_origin[999] = {0};
+
+            int32_t count = fread(row_origin,1,999,origin_bmp);
+            fwrite(row_origin,1,count,distort_bmp);
+        }
+
+        return 0;
+    }
+
+    double m = 0;
+    m = tan(ang * M_PI / 180.0);
+
+    fread(&header,sizeof(sHeader),1,origin_bmp);
+    shift = (int32_t)(header.height * (1.0 / m));
+    header.width += shift;
+    fwrite(&header,sizeof(sHeader),1,distort_bmp);
+
+    int32_t col_origin = 0;
+    int32_t col_modified = 0;
+    uint8_t* row_origin = NULL;
+    uint8_t* row_modified = NULL;
+    col_origin = (header.width - shift) * 3 + (header.width - shift) % 4;
+    col_modified = header.width * 3 + header.width % 4;
+    row_origin = (uint8_t*)malloc(sizeof(uint8_t) * col_origin);
+    row_modified = (uint8_t*)malloc(sizeof(uint8_t) * col_modified);
+
+    for(int32_t i = 0;i < header.height;i++){
+        fread(row_origin,1,col_origin,origin_bmp);
+        for(int32_t j = 0;j < col_modified;j++) row_modified[j] = 255;
+        for(int32_t j = 0;j < col_modified;j = j + 3){
+            if(m * j / 3 - i >= 0.0){
+                // printf("%d %d\n",i,j);
+                for(int32_t k = 0;k < col_origin;k++) row_modified[j+k] = row_origin[k];
+                fwrite(row_modified,1,col_modified,distort_bmp);
+                break;
+            }
+        }
+    }
+
+    fclose(origin_bmp);
+    fclose(distort_bmp);
+
+    return 0;
+}
--- hw03/test.c
+++ hw03/test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
 int main(){
     char* str = "11/08/17,Arsenal,Leicester,4,3,H,2,2,D,M Dean,27,6,10,3,9,12,9,4,0,1,0,0,1.53,4.5,6.5,1.5,4.6,6.75,1.47,4.5,6.5,1.44,4.4,6.5,1.53,4.55,6.85,1.53,4.2,6,1.53,4.5,6.5,41,1.55,1.51,4.6,4.43,6.89,6.44,37,1.65,1.61,2.43,2.32,21,-1,1.91,1.85,2.1,2.02,1.49,4.73,7.25";
@@ -17,6 +18,8 @@ int main(){
     sscanf(str,"%*[^,],%[^,],%[^,],%d,%d,%c,%*[^,],%*[^,],%*[^,],%*[^,],%d,%d,%d,%d,%*[^,],%*[^,],%*[^,],%*[^,],%*[^,],%*[^,],%d,%d,%*s",homeName,awayName,&FTHG,&FTAG,&FTR,&HS,&AS,&HST,&AST,&HR,&AR);
 
     printf("%s %s %d %d %c %d %d %d %d %d %d\n",homeName,awayName,FTHG,FTAG,FTR,HS,AS,HST,AST,HR,AR);
+
+    printf("%lf\n",cos(M_PI));
 
     return 0;
 }
