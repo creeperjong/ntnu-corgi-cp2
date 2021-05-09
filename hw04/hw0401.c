986ef2b494981b52fe3ab4e38d4c805e6b999031
creeperjong <leogood50203@gmail.com>
2021-05-09 23:31:58 +0800

    init

---
+++ hw04/hw0401.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <math.h>
+
+struct option long_options[] = {
+    {"size", 1, NULL, 'z'},
+    {"help", 0, NULL, 'h'},
+    {0, 0, 0, 0}
+};
+
+int main(int argc, char *argv[]){
+
+    int32_t c = 0;
+    int32_t ridx = 0;
+    int32_t opt_s = 0;
+    int32_t opt_r = 0;
+    int32_t opt_size = 0;
+    int32_t opt_help = 0;
+    FILE* file_origin = NULL;
+    char filename_origin[32] = "";
+    char optarg_r[32] = "";
+    char input_size[32] = "";
+
+    printf("%d\n",optind);
+     
+    while( (c = getopt_long(argc,argv,"s:r:",long_options,NULL)) != -1 ){
+        switch(c){
+            case 's':
+                opt_s = 1;
+                strncpy(filename_origin,optarg,32);
+                break;
+            case 'r':
+                opt_r = 1;
+                ridx = optind - 1;
+                break;
+            case 'z':
+                opt_size = 1;
+                strncpy(input_size,optarg,32);
+                break;
+            case 'h':
+                opt_help = 1;
+                break;
+        }
+    }
+    
+    uint32_t num = 0;
+    int64_t filesize = 0;
+    uint64_t size = 1000;
+    char** garbage = NULL;
+    char filename_modified[32] = "";
+    FILE* file_modified = NULL;
+
+    if(opt_size) size = (uint64_t)strtol(input_size,garbage,10);
+
+    if(opt_s){
+
+        if((file_origin = fopen(filename_origin,"rb")) == NULL){
+            perror("Error");
+            return 0;
+        }
+        
+        fseek(file_origin,0,SEEK_END);
+        filesize = ftell(file_origin);
+        rewind(file_origin);
+
+        num = (uint32_t)ceil(((double)filesize / size));
+
+        uint8_t* buf = NULL;
+        buf = (uint8_t*)malloc(sizeof(uint8_t) * size);
+
+        for(uint32_t i = 1;i <= num;i++){
+
+            int32_t cnt = 0;
+
+            strncpy(filename_modified,filename_origin,32);
+            filename_modified[strlen(filename_modified)] = '.';
+            snprintf(&filename_modified[strlen(filename_modified)],31 - strlen(filename_modified),"%u",i);
+
+            file_modified = fopen(filename_modified,"wb");
+
+            uint8_t offset = strlen(filename_origin) + 6;
+            
+            fwrite(&offset,sizeof(offset),1,file_modified);
+            fwrite(&filename_origin,strlen(filename_origin) + 1,1,file_modified);
+            fwrite(&i,sizeof(i),1,file_modified);
+
+            cnt = fread(buf,1,size,file_origin);
+            fwrite(buf,1,cnt,file_modified);
+
+            printf("%s ",filename_modified);
+
+            fclose(file_modified);
+
+        }
+        printf("\n");
+
+        free(buf);
+        fclose(file_origin);
+
+    }
+
+    printf("%d\n",optind);
+
+    if(opt_r){
+        
+    }
+
+
+
+
+
+    return 0;
+}
